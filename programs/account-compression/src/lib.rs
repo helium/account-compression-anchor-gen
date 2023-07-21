1c20abe6b7ebaf74fc1cdc20cb728c49ed382e70
Chewing Glass <chewinglass123@gmail.com>
2023-07-21 11:19:35 -0500

    Add noop

--- programs/account-compression/src/lib.rs
+++ programs/account-compression/src/lib.rs
@@ -1,5 +1,15 @@
+use std::str::FromStr;
+
 use anchor_lang::prelude::*;
 
 anchor_gen::generate_cpi_crate!("./idl.json");
 
 declare_id!("cmtDvXumGCrqC1Age74AVPhSRVXJMd8PJS91L8KbNCK");
+
+struct Noop {}
+
+impl Owner for Noop {
+    fn owner() -> Pubkey {
+        Pubkey::from_str("noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV").unwrap()
+    }
+}
