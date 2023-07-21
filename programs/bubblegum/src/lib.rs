62684aa6060616eef22a99cb7604d2894b564088
Chewing Glass <chewinglass123@gmail.com>
2023-07-21 11:57:47 -0500

    Add leaf schema

--- programs/bubblegum/src/lib.rs
+++ programs/bubblegum/src/lib.rs
@@ -17,3 +17,61 @@ pub fn get_asset_id(tree_id: &Pubkey, nonce: u64) -> Pubkey {
     )
     .0
 }
+
+impl Default for LeafSchema {
+    fn default() -> Self {
+        Self::V1 {
+            id: Default::default(),
+            owner: Default::default(),
+            delegate: Default::default(),
+            nonce: 0,
+            data_hash: [0; 32],
+            creator_hash: [0; 32],
+        }
+    }
+}
+
+impl LeafSchema {
+    pub fn new_v0(
+        id: Pubkey,
+        owner: Pubkey,
+        delegate: Pubkey,
+        nonce: u64,
+        data_hash: [u8; 32],
+        creator_hash: [u8; 32],
+    ) -> Self {
+        LeafSchema::V1 {
+            id,
+            owner,
+            delegate,
+            nonce,
+            data_hash,
+            creator_hash,
+        }
+    }
+
+    pub fn version(&self) -> Version {
+        match self {
+            LeafSchema::V1 { .. } => Version::V1,
+        }
+    }
+
+    pub fn id(&self) -> Pubkey {
+        match self {
+            LeafSchema::V1 { id, .. } => *id,
+        }
+    }
+
+    pub fn nonce(&self) -> u64 {
+        match self {
+            LeafSchema::V1 { nonce, .. } => *nonce,
+        }
+    }
+
+    pub fn data_hash(&self) -> [u8; 32] {
+        match self {
+            LeafSchema::V1 { data_hash, .. } => *data_hash,
+        }
+    }
+}
+
