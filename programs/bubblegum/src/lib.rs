2490ca17ff82d1e0785118420c3902d4b1901dbc
Chewing Glass <chewinglass123@gmail.com>
2023-07-21 11:15:15 -0500

    Add get asset id

--- programs/bubblegum/src/lib.rs
+++ programs/bubblegum/src/lib.rs
@@ -3,3 +3,17 @@ use anchor_lang::prelude::*;
 anchor_gen::generate_cpi_crate!("./idl.json");
 
 declare_id!("BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY");
+
+pub const ASSET_PREFIX: &str = "asset";
+
+pub fn get_asset_id(tree_id: &Pubkey, nonce: u64) -> Pubkey {
+    Pubkey::find_program_address(
+        &[
+            ASSET_PREFIX.as_ref(),
+            tree_id.as_ref(),
+            &nonce.to_le_bytes(),
+        ],
+        &crate::id(),
+    )
+    .0
+}
